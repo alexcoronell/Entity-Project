eb83be8e543e057e0691a5ed04aab833ae923da8
Alex Coronell <alexcoronell@gmail.com>
2024-08-01 11:30:58 -0500

    Fluent Api applied con TasksContext

--- Models/Task.cs
+++ Models/Task.cs
@@ -5,21 +5,21 @@ namespace projectef.Models
 {
     public class Task
     {
-        [Key]
+        //[Key]
         public Guid TaskId { get; set; }
 
-        [ForeignKey("CategoryId")]
+        //[ForeignKey("CategoryId")]
         public Guid CategoryId { get; set; }
 
-        [Required]
-        [MaxLength(200)]
+        //[Required]
+        //[MaxLength(200)]
         public string Title { get; set; }
         public string Description { get; set; }
         public Priority PriorityTask { get; set; }
         public DateTime DateCreated { get; set; }
         public virtual required Category Category { get; set; }
 
-        [NotMapped]
+        //[NotMapped]
         public string Resume {  get; set; }
     }
 }
--- TasksContext.cs
+++ TasksContext.cs
@@ -9,5 +9,28 @@ namespace projectef
         public DbSet<Category> Categories { get; set; }
 
         public TasksContext(DbContextOptions<TasksContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>(category =>
+            {
+                category.ToTable("Category");
+                category.HasKey(c => c.CategoryId);
+                category.Property(c => c.Name).IsRequired().HasMaxLength(150);
+                category.Property(c => c.Description);
+            });
+
+            modelBuilder.Entity<Models.Task>(task =>
+            {
+                task.ToTable("Task");
+                task.HasKey(t => t.TaskId);
+                task.HasOne(c => c.Category).WithMany(t => t.Tasks).HasForeignKey(c =>  c.CategoryId);
+                task.Property(t => t.Title).IsRequired().HasMaxLength(200);
+                task.Property(t => t.Description);
+                task.Property(t => t.PriorityTask);
+                task.Property(t => t.DateCreated);
+                task.Ignore(t => t.Resume);
+            });
+        }
     }
 }
